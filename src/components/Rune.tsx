9d28eefb545ec7801cb59d1a98c47516a148f484
Hongyi Syue <homeyxue@outlook.com>
2024-12-19 16:16:25 -0800

    onLearn

--- src/components/Rune.tsx
+++ src/components/Rune.tsx
@@ -2,14 +2,15 @@ import { MouseEvent, useState } from "react";
 import { TPointUpdate, TRune } from "../utils/types";
 
 function Rune(rune: TRune, onLearn: (change: TPointUpdate) => any) {
-    const [learned, setLearned] = useState(false);
+    const [learned, setLearned] = useState(rune.learned);
 
     function handleClick(event: MouseEvent) {
         switch(event.button) {
             case 0:
-                //left click
+                //left click to learn
                 if (rune.learnable && !rune.learned) {
                     setLearned(true);
+                    rune.learned = true;
                     onLearn({
                         rune: rune,
                         point_change: -1
@@ -17,13 +18,21 @@ function Rune(rune: TRune, onLearn: (change: TPointUpdate) => any) {
                 }
                 break;
             case 2: 
-                //right click
+                //right click to unlearn
+                //note: 
+                // 1. if the rune has not been learned, unlearn is not allowed
+                // 2. if the next rune has been learned, unlearn is not allowed
                 if (rune.learned) {
-                    setLearned(false);
-                    onLearn({
-                        rune: rune,
-                        point_change: 1
-                    });
+                    if (rune.next_rune?.learned) {
+                        break;
+                    } else {
+                        setLearned(false);
+                        rune.learned = false;
+                        onLearn({
+                            rune: rune,
+                            point_change: 1
+                        });
+                    }
                 }
                 break;
         }
--- src/components/RunePath.tsx
+++ src/components/RunePath.tsx
@@ -1,5 +1,27 @@
-import { TRunePath } from "../utils/types";
+import { TPointUpdate, TRune, TRunePath } from "../utils/types";
 
-function RunePath(props: TRunePath) {
-    
+function RunePath(runes: TRune[], onPointUpdate: (change: number) => number) {
+    function hanleLearning(change: TPointUpdate) {
+        /**
+         * hanle learning rune should do the following things:
+         * 1. update point usage
+         * 2. update learnable of next rune
+         */
+        const remainPoint = onPointUpdate(change.point_change);
+        switch(change.point_change) {
+            case -1:
+                //learn a rune
+                if (change.rune.next_rune && remainPoint > 1) {
+                    change.rune.next_rune.learnable = true;
+                }
+                break;
+            case 1:
+                //unlearn a rune
+                if (change.rune.next_rune) {
+                    change.rune.next_rune.learnable = false;
+                }
+                break;
+
+        }
+    }
 }
