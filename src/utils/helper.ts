7813586df42fd321511db68ed8b411f76a704b13
Hongyi Syue <homeyxue@outlook.com>
2024-12-19 18:03:18 -0800

    catch up change

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -7,15 +7,18 @@ export function pathBuilder(runes: TRune[]): TRunePath {
      * 2. the id of the RunePath depends on the order of the passed in array (so that it can be unique but not random)
      */
     if (runes?.length > 0) {
+        //set up the first rune
         let pathId = `p${runes[0].id}`;
         const pathRunes = [runes[0]];
-
+        
+        //connect the rest of the rune
         for (let i=1; i<runes.length; i++) {
             pathId += `-${runes[i].id}`;
-            runes[i].prereq_id = runes[i-1].id;
-            runes[i-1].next_rune_id = runes[i].id;
+            runes[i].prereq_rune = runes[i-1];
+            runes[i-1].next_rune = runes[i];
             pathRunes.push(runes[i]);
         }
+
         return {
             id: pathId,
             runes: pathRunes
